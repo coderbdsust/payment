c15fcaf3d3a52e875536cae8b1ee8cedbec83ec4
Biswajit Debnath <biswajit@kinetik.care>
2019-04-15 22:00:31 +0600

    send money ui refactored

--- src/main/java/com/dbbl/payment/controller/LoginController.java
+++ src/main/java/com/dbbl/payment/controller/LoginController.java
@@ -10,4 +10,9 @@ public class LoginController {
     public String loginPage(){
         return "login";
     }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "redirect:/login";
+    }
 }
