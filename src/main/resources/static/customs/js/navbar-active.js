f87a2931f35b721359f367a72594c65a16a8d91c
Biswajit Debnath <biswajit@kinetik.care>
2019-04-22 11:37:15 +0600

    sidebar active class aded

---
+++ src/main/resources/static/customs/js/navbar-active.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    var url      = window.location.href;
+    $('.nav li.active').removeClass('active');
+    var sidebarUL = $(".sidebar-wrapper ul>li");
+   $.each(sidebarUL, function(i, li){
+       var a = li.children[0];
+       var href = a.href;
+       if(href===url){
+           li.classList.add("active");
+           return ;
+       }
+   });
+});
